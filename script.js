b5c3b8d1344f751c57ea466af77f62fa5957f158
dorrolfozongalez01 <villamarquezalexis@gmail.com>
2025-04-09 13:12:50 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuToggle = document.getElementById("menu-toggle");
+
+    const menu = document.getElementById("menu");
+
+
+
+    menuToggle.addEventListener("click", function () {
+
+        menu.classList.toggle("active");
+
+    });
+
+});
