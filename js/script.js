88bea9cc2bd09e8603e013c6879a03799524c6d3
dorrolfozongalez01 <villamarquezalexis@gmail.com>
2025-06-05 10:00:32 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+document.addEventListener("mousemove", function (e) {
+    const x = e.clientX / window.innerWidth * 100;
+    const y = e.clientY / window.innerHeight * 100;
+    document.body.style.background = `radial-gradient(circle at ${x}% ${y}%, #ff7e5f, #feb47b)`;
+});
 
